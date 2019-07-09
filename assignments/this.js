6f2054bf907e0a49ed1af4fa0d3cd08850ea765d
roselandroche <r.landroche@gmail.com>
2019-07-08 21:42:39 -0400

    Working on questions

--- assignments/this.js
+++ assignments/this.js
@@ -1,8 +1,8 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
+* 1. If you don't intentionally bind 'this' to a thing, it will automatically be tied to the global window/console.
+* 2. When using dot notation, 'this' refers to the object to the left of the dot.
 * 3. 
 * 4. 
 *
@@ -13,9 +13,14 @@
 
 // code example for Window Binding
 
+// Whoops forgot to tie the code to a thing...
+// console.log(this);
+
 // Principle 2
 
 // code example for Implicit Binding
+
+
 
 // Principle 3
 
