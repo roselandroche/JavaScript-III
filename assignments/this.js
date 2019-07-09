712429c889e0234919315ff53e2ef4237d942e02
roselandroche <r.landroche@gmail.com>
2019-07-08 21:47:17 -0400

    Working on questions

--- assignments/this.js
+++ assignments/this.js
@@ -3,7 +3,7 @@
 *
 * 1. If you don't intentionally bind 'this' to a thing, it will automatically be tied to the global window/console.
 * 2. When using dot notation, 'this' refers to the object to the left of the dot.
-* 3. 
+* 3. When using 'this' with a constructor, it refers to the specific iteration you just made (as opposed to the one it inherits stuff from)
 * 4. 
 *
 * write out a code example of each explanation above
@@ -19,7 +19,9 @@
 // Principle 2
 
 // code example for Implicit Binding
-
+// anObj ... ... ...
+//          ... ... this.name ... ... 
+// 'this' refers to anObj
 
 
 // Principle 3
