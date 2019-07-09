67cd7e2f3c8ecb4373611682c10fd1c59ab0fb84
roselandroche <r.landroche@gmail.com>
2019-07-09 18:57:27 -0400

    Finished questions

--- assignments/this.js
+++ assignments/this.js
@@ -2,9 +2,12 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. If you don't intentionally bind 'this' to a thing, it will automatically be tied to the global window/console.
+
 * 2. When using dot notation, 'this' refers to the object to the left of the dot.
+
 * 3. When using 'this' with a constructor, it refers to the specific iteration you just made (as opposed to the one it inherits stuff from)
-* 4. 
+
+* 4. You can use 'this' to bind things (functions or properties) to an object prototype after the fact.
 *
 * write out a code example of each explanation above
 */
@@ -13,21 +16,33 @@
 
 // code example for Window Binding
 
-// Whoops forgot to tie the code to a thing...
-// console.log(this);
+// This code hasn't been tied to anything, it will be tied to the window by default.
+console.log(this);
 
 // Principle 2
 
 // code example for Implicit Binding
-// anObj ... ... ...
-//          ... ... this.name ... ... 
-// 'this' refers to anObj
+//
+// In this example 'this' refers to the person
 
+function Person(attr) {
+    this.name = attr.name;
+    console.log(`This is ${this.name}`);
+}
 
 // Principle 3
 
 // code example for New Binding
 
+const sarah = new Person({name: 'Sarah'}); 
+
+
+// 'this' refers to Sarah
+
 // Principle 4
 
 // code example for Explicit Binding
+
+Person.prototype.hello = function() {
+    console.log(`Hello ${this.name}!`);
+};
